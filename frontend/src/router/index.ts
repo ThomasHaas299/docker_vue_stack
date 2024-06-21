bf4e5a9b089bf81bb23157e404710ef599963709
ThomasHaas299 <github@thomashaas.de>
2024-06-21 16:52:20 +0200

    Ein ganz einfaches Backend ist jetzt vorhanden.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Playground01View from '../views/Playground01View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/playground01',
+      name: 'playground01',
+      component: Playground01View
     },
     {
       path: '/about',
---
+++ frontend/src/views/Playground01View.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="randomElement">
+    <p>{{ randomElement.joke }}</p>
+    <button @click="selectRandomElement">nächster ...</button>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { BACKEND_URL } from '@/config'
+
+const dataArray = ref([])
+const randomElement = ref(null)
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get(BACKEND_URL)
+    dataArray.value = response.data
+    selectRandomElement()
+  } catch (error) {
+    console.error('Fehler beim Abrufen der Daten:', error)
+  }
+}
+
+const selectRandomElement = () => {
+  const length = dataArray.value.length
+
+  if (dataArray.value.length > 0) {
+    const randomIndex = Math.floor(Math.random() * dataArray.value.length)
+    randomElement.value = dataArray.value[randomIndex]
+  }
+}
+
+onMounted(fetchData)
+</script>
